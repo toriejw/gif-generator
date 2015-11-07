e15d6d388731532f9b50b0f3a249709ee07f773b
Torie Joy-Warren <victoria.joywarren@gmail.com>
2015-11-07 13:41:58 -0700

    Add error messages for invalid logins to views and session controller

--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -2,23 +2,47 @@ require 'test_helper'
 
 class UserLoginTest < ActionDispatch::IntegrationTest
 
-def create_user
-  User.create(username: "Leon", password: "password")
-end
+  def create_user
+    User.create(username: "Leon", password: "password")
+  end
 
-test "registered user can login with correct username and password" do
-  create_user
-  visit '/'
+  test "registered user can login with correct username and password" do
+    create_user
+    visit '/'
 
-  click_link "Login"
-  assert_equal login_path, current_path
+    click_link "Login"
+    assert_equal login_path, current_path
 
-  fill_in "Username", with: "Leon"
-  fill_in "Password", with: "password"
-  click_button "Login"
+    fill_in "Username", with: "Leon"
+    fill_in "Password", with: "password"
+    click_button "Login"
 
-  assert_equal user_path(User.find_by(username: "Leon")), current_path
-  assert page.has_content?("Leon's Favourites")
-end
+    assert_equal user_path(User.find_by(username: "Leon")), current_path
+    assert page.has_content?("Leon's Favourites")
+  end
+
+  test "registered user can't login with wrong password" do
+    create_user
+    visit login_path
+
+    fill_in "Username", with: "Leon"
+    fill_in "Password", with: "wrongpassword"
+    click_button "Login"
+
+    assert_equal login_path, current_path
+    assert page.has_content?("Invalid login")
+  end
+
+  test "unregistered user can't login" do
+    create_user
+    visit login_path
+
+    fill_in "Username", with: "Samantha"
+    fill_in "Password", with: "password"
+    click_button "Login"
+
+    assert_equal login_path, current_path
+    assert page.has_content?("Invalid login")
+  end
 
 end
